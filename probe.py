1a6aa08184d9dacdfd81e9c20dfb3d6756efff6a
evgenii <evgenij-pigalin@yandex.ru>
2023-06-04 12:34:36 +0400

    добавление файлов 04.06.2023

---
+++ probe.py
@@ -0,0 +1,15 @@
+# Python. Урок 2
+# TODO - list и tuple
+# TODO - условный оператор if
+# TODO - операторы цикла for и white
+# TODO - лаконичная запись циклов
+
+# Типы данных
+x = 1 # числа
+world = "Hello" # строки
+world = "Hello" # строки
+world = "Hello" # строки
+world = "Hello" # строки
+print (2+2)
+
+
